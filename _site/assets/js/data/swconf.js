67edfb281fee4f178e67c753efbd2669c1a1cd7c
choijino <citopes@gmail.com>
2025-08-07 14:29:57 +0900

    ignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754543814',resources: [
+    cacheName: 'chirpy-1754544567',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
