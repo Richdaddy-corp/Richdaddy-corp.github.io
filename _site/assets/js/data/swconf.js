e96627d5371cbe488eda2ee1efb3916f0029f77b
choijino <citopes@gmail.com>
2025-08-07 14:16:03 +0900

    20250807 gitignore update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754543329',resources: [
+    cacheName: 'chirpy-1754543676',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
